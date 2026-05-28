8d8785e3c6815590e3df017e9fe3aa8a2051b72d
mohit <mohitgora1008@gmail.com>
2026-05-29 04:42:21 +0530

    your commit message

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// ✅ fallback added (VERY IMPORTANT)
+// fallback added (VERY IMPORTANT)
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   const api = axios.create({
