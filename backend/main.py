2336953045a6da440a1602386debe7822b269546
mohit <mohitgora1008@gmail.com>
2026-05-29 08:33:04 +0530

    fix: update production frontend URLs

--- backend/main.py
+++ backend/main.py
@@ -84,7 +84,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ai-resume-analyzer-and-job-matcher-6.onrender.com",
+        "https://ai-resume-analyzer-and-job-matcher-xi.vercel.app/",
         "http://localhost:5173"
     ],
     allow_credentials=True,
